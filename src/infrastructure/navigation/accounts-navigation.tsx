35937201dc8d5387e3ca76809b3d78975d6a5908
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-07-15 18:30:34 +0800

    feat (update): Add ForgotPasswordComponent function that returns ForgotPassword component screen

--- src/infrastructure/navigation/accounts-navigation.tsx
+++ src/infrastructure/navigation/accounts-navigation.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../../features/accounts/screens/sign-in';
+import ForgotPassword from '../../features/accounts/screens/forgot-password';
 
 const Stack = createNativeStackNavigator();
 
 function SignInComponent() {
   return <SignIn />;
+}
+
+function ForgotPasswordComponent() {
+  return <ForgotPassword />;
 }
 
 export default function AccountsNavigation(): React.JSX.Element {
@@ -16,6 +21,10 @@ export default function AccountsNavigation(): React.JSX.Element {
         animation: 'fade',
       }}>
       <Stack.Screen name="Sign In" component={SignInComponent} />
+      <Stack.Screen
+        name="Forgot Password"
+        component={ForgotPasswordComponent}
+      />
     </Stack.Navigator>
   );
 }
