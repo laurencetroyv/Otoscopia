217d97176b45f39379e34f4b13e5cc50695bc246
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-07-15 18:54:25 +0800

    feat (update): Add SignUpComponent function that returns SignUp component screen

--- src/infrastructure/navigation/accounts-navigation.tsx
+++ src/infrastructure/navigation/accounts-navigation.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../../features/accounts/screens/sign-in';
 import ForgotPassword from '../../features/accounts/screens/forgot-password';
+import SignUp from '../../features/accounts/screens/sign-up';
 
 const Stack = createNativeStackNavigator();
 
 function SignInComponent() {
   return <SignIn />;
+}
+
+function SignUpComponent() {
+  return <SignUp />;
 }
 
 function ForgotPasswordComponent() {
@@ -21,6 +26,7 @@ export default function AccountsNavigation(): React.JSX.Element {
         animation: 'fade',
       }}>
       <Stack.Screen name="Sign In" component={SignInComponent} />
+      <Stack.Screen name="Sign Up" component={SignUpComponent} />
       <Stack.Screen
         name="Forgot Password"
         component={ForgotPasswordComponent}
