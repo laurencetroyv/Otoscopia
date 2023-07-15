821cea8e85ffd52ce08a8fca0cb4060702a9fc0e
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-07-15 17:51:15 +0800

    chore (update): Added error code auth/unknown and auth/network-request-failed

--- src/services/authentication-service.js
+++ src/services/authentication-service.js
@@ -8,5 +8,11 @@ export const signInRequest = error => {
   if (error.code === 'auth/user-not-found') {
     return 'There is no user record corresponding to this email address, Please create a new account.';
   }
+  if (error.code === 'auth/unknown') {
+    return 'Unknown error occurred, please try again later'
+  }
+  if (error.code === 'auth/network-request-failed') {
+    return 'Please check your internet connection'
+  }
   return error.code;
 };
